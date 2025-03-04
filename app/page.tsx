50996c93d3bc1c11437a9ec78b59ad16e1ef35cc
Saiful Islam <saiful.islam@solar-ict.com>
2025-03-04 22:41:05 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+<h1>Alhamdulillah, Jenkins works good</h1>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
