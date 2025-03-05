9dec7e5aa14065cf0023d38ae324f10697c4f86d
Saiful Islam <saiful.islam@solar-ict.com>
2025-03-05 12:36:32 +0100

    color Changed

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
 <h1> SubhanAllah</h1>
 <p> This a test phrase to show Tangila today </p>
-
+<h1> we changed the color</h1>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
